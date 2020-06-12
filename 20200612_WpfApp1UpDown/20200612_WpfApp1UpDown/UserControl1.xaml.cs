f5faaf31774ebd1e5e51554751daf8142ff82d80
waten <gogowaten@yahoo.co.jp>
2020-06-12 15:39:14 +0900

    202006121539

---
+++ 20200612_WpfApp1UpDown/20200612_WpfApp1UpDown/UserControl1.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20200612_WpfApp1UpDown
+{
+    /// <summary>
+    /// UserControl1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(int), typeof(UserControl1), new PropertyMetadata(0));
+
+        public int Value
+        {
+            get { return (int)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        private void UpButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Value++;
+        }
+
+        private void DownButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Value--;
+        }
+
+    }
+}
