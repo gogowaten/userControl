a6e9b141f1278da70adc4b3a314fabf874e3b764
waten <gogowaten@yahoo.co.jp>
2020-06-13 16:45:46 +0900

    202006131645

---
+++ 20200613_CoreWpfControlLibrary1/20200613_CoreWpfControlLibrary1/UserControl1.xaml.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20200613_CoreWpfControlLibrary1
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+    "Value",
+    typeof(int),
+    typeof(UserControl1),
+    new PropertyMetadata(0));
+
+        public int Value
+        {
+            get { return (int)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        private void UpButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Value++;
+        }
+
+        private void DownButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Value--;
+        }
+    }
+}
